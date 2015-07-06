a33aa8f6b614beedd620bd90f128a4010af992fd
oscar2d2 <oscarkong@gmail.com>
2015-07-06 19:48:55 +0800

    Ch16 ex2

--- lib/ch16.ex
+++ lib/ch16.ex
@@ -1,11 +1,16 @@
 defmodule Ch16 do
 
-  #OTP-Servers-1
   defmodule MyStack do
     use GenServer
 
+    #OTP-Servers-1
     def handle_call(:pop, _from, [ head | tail]) do
       {:reply, head, tail} 
+    end
+
+    #OTP-Servers-2
+    def handle_cast({:push, value}, current_stack) do
+      {:noreply, [ value | current_stack]}
     end
   end
 end
