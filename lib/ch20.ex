dd236ffa2516b1e119ab23bff54a87690ee79bf5
oscar2d2 <oscarkong@gmail.com>
2015-07-11 15:30:51 +0800

    ch20 ex2

--- lib/ch20.ex
+++ lib/ch20.ex
@@ -13,11 +13,30 @@ defmodule My do
   end
 end
 
+defmodule Times do
+  #MacrosAndCodeEvaluation-2
+  defmacro times_n(times) do
+    quote do
+      def unquote(:"times_#{times}")(n) do
+        unquote(times) * n
+      end
+    end
+  end
+end
+
 defmodule Test do
   require My
+  require Times
+  
   My.unless 1==2 do
     IO.puts "1 != 2"
   else
     IO.puts "1 == 2"
   end
+
+  Times.times_n(3)
+  Times.times_n(4)
 end
+
+IO.puts Test.times_3(4)
+IO.puts Test.times_4(5)
